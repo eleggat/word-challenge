e5336c7e1400ff04add1fe5bc984f1568033b21b
eleggat <eleggat2@gmail.com>
2025-05-22 12:35:25 -0400

    adding words and letters/points

--- word-challenge/game.py
+++ word-challenge/game.py
@@ -5,7 +5,6 @@ Script for the word challenge game
 """
 
 """
-- Get a valid word list
 
 - Create a list of letters with points: 16 outer, 8 middle, 1 inner
 	- Probability distribution so that vowels and common consonants are more common
@@ -24,14 +23,34 @@ Script for the word challenge game
 
 
 # Imports
-
+import csv
 
 
 # Objects
 
-#word list goes here
-#dictionary of letters and point values goes here
-#empty letter grid goes here
+#word list
+with open('scrabble-words.txt', 'r') as infile:
+	words_dirty = infile.read()
+words = words_dirty.split("\n")[2:]
+
+#dictionary of letters and point values {letter : [outer, middle, inner]}
+with open('../word-challenge/letter-points.csv', mode='r', encoding='utf-8-sig') as infile:
+	reader = csv.reader(infile)
+	letters = {rows[0]:rows[1:4] for rows in reader}
+
+for points in letters.values(): #make sure all points are integers
+	for i in range(len(points)):
+		points[i] = int(points[i])
+
+#letter probabilities
+inverses = {letter: 1 / points[0] for letter, points in letters.items()}
+inverses_total = sum(inverses.values())
+letters_probs = {letter: inv / inverses_total for letter, inv in inverses.items()}
+
+#empty letter rings
+outer = [None] * 16
+middle = [None] * 8
+inner = [None]
 
 
 # Functions
